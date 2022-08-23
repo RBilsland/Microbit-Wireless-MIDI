45dac9e199988ebf1cf7698fcbe273293c1dce2f
Robert Bilsland <RBilsland@users.noreply.github.com>
2022-08-23 10:58:48 +0100

    Initial commit

---
+++ MakeCode-Extension/MIDI-Messages.ts
@@ -0,0 +1,255 @@
+/**
+ * Construct MIDI messages that can be sent to Node RED for forwarding to MIDI devices.
+ */
+
+enum NoteSelection {
+    B = 11,
+    //% block="A#"
+    As = 10,
+    A = 9,
+    //% block="G#"
+    Gs = 8,
+    G = 7,
+    //% block="F#"
+    Fs = 6,
+    F = 5,
+    E = 4,
+    //% block="D#"
+    Ds = 3,
+    D = 2,
+    //% block="C#"
+    Cs = 1,
+    C = 0
+}
+
+enum OctaveSelection {
+    //% block="9"
+    O9 = 9,
+    //% block="8"
+    O8 = 8,
+    //% block="7"
+    O7 = 7,
+    //% block="6"
+    O6 = 6,
+    //% block="5"
+    O5 = 5,
+    //% block="4"
+    O4 = 4,
+    //% block="3"
+    O3 = 3,
+    //% block="2"
+    O2 = 2,
+    //% block="1"
+    O1 = 1,
+    //% block="0"
+    O0 = 0,
+    //% block="-1"
+    Om1 = -1
+}
+
+enum VelocitySelection {
+    none = 0,
+    pppp = 8,
+    ppp = 20,
+    pp = 31,
+    p = 42,
+    mp = 53,
+    mf = 64,
+    f = 80,
+    ff = 96,
+    fff = 112,
+    ffff = 127
+}
+
+enum MIDIMessageType {
+    NoteOff = 128,
+    NoteOn = 144,
+    Aftertouch = 160,
+    ControlChange = 176,
+    ProgramChange = 192,
+    ChannelPresure = 208,
+    PitchBend = 224
+}
+
+//% color="#daa520" weight=90 icon="\uf001" block="MIDI Messages"
+namespace midiMessages {
+    let globalChannel = 10;
+
+    function validateRange(value: number, lowerLimit: number, upperLimit: number): number {
+        if (value < lowerLimit) {
+            value = lowerLimit;
+        }
+        if (value > upperLimit) {
+            value = upperLimit;
+        }
+        return value;
+    }
+
+    function validateChannel(channel: number): number {
+        if (channel === null) {
+            return validateRange(globalChannel, 1, 16);
+        } else {
+            return validateRange(channel, 1, 16);
+        }
+    }
+
+    function validateNote(note: number): number {
+        return validateRange(note, 0, 127);
+    }
+
+    function validateVelocity(velocity: number): number {
+        return validateRange(velocity, 0, 127);
+    }
+
+    function validatePressure(pressure: number): number {
+        return validateRange(pressure, 0, 127);
+    }
+
+    function validateControlNumber(controlNumber: number): number {
+        return validateRange(controlNumber, 0, 127);
+    }
+
+    function validateControlValue(controlValue: number): number {
+        return validateRange(controlValue, 0, 127);
+    }
+
+    function validateProgramNumber(programNumber: number): number {
+        return validateRange(programNumber, 0, 127);
+    }
+
+    function validatePitch(pitch: number): number {
+        return validateRange(pitch, 0, 16383);
+    }
+
+    //% blockId="noteSelection_conv" block="%note"
+    //% blockHidden=true
+    //% note.defl=NoteSelection.C
+    export function noteSelection(note: NoteSelection): number {
+        return note
+    }
+
+    //% blockId="octaveSelection_conv" block="%octave"
+    //% blockHidden=true
+    //% octave.defl=OctaveSelection.O4
+    export function octaveSelection(octave: OctaveSelection): number {
+        return octave
+    }
+
+    //% blockId="velocitySelection_conv" block="%velocity"
+    //% blockHidden=true
+    //% velocity.defl=VelocitySelection.mf
+    export function velocitySelection(velocity: VelocitySelection): number {
+        return velocity
+    }
+
+    //% block="play $note=noteSelection_conv||octave $octave=octaveSelection_conv"
+    //% group="Notes"
+    //% weight=20
+    //% expandableArgumentMode="enable"
+    export function fromNoteAndOctave(note: number, octave: number = OctaveSelection.O4): number {
+        let midiNote = (octave * 12) + 12 + note;
+
+        return validateNote(midiNote);
+    }
+
+    //% block="frequency $frequency"
+    //% group="Notes"
+    //% weight=10
+    export function fromFrequency(frequency: number): number {
+        return Math.round(12 * (Math.log(frequency / 220) / Math.log(2)) + 57);
+    }
+
+    //% block="note off $note||velocity $velocity=velocitySelection_conv|channel $channel"
+    //% group="Commands"
+    //% weight=70
+    //% expandableArgumentMode="enabled"
+    //% note.defl=60
+    //% volocity.defl=VelocitySelection.mf
+    //% channel.defl=1
+    export function noteOff(note: number, velocity: number = VelocitySelection.mf, channel: number = null): string {
+        return "[" + (MIDIMessageType.NoteOff + validateChannel(channel) - 1) + "," + validateNote(note) + "," + validateVelocity(velocity) + "]";
+    }
+
+    //% block="note on $note||velocity $velocity=velocitySelection_conv|channel $channel"
+    //% group="Commands"
+    //% weight=60
+    //% expandableArgumentMode="enabled"
+    //% note.defl=60
+    //% volocity.defl=VelocitySelection.mf
+    //% channel.defl=1
+    export function noteOn(note: number, velocity: number = VelocitySelection.mf, channel: number = null): string {
+        return "[" + (MIDIMessageType.NoteOn + validateChannel(channel) - 1) + "," + validateNote(note) + "," + validateVelocity(velocity) + "]";
+    }
+
+    //% block="aftertouch $note pressure $pressure||channel $channel"
+    //% group="Commands"
+    //% weight=50
+    //% expandableArgumentMode="enabled"
+    //% note.defl=60
+    //% pressure.defl=63
+    //% channel.defl=1
+    export function aftertouch(note: number, pressure: number, channel: number = null): string {
+        return "[" + (MIDIMessageType.Aftertouch + validateChannel(channel) - 1) + "," + validateNote(note) + "," + validatePressure(pressure) + "]";
+    }
+
+    //% block="control change $number value $value||channel $channel"
+    //% group="Commands"
+    //% weight=40
+    //% expandableArgumentMode="enabled"
+    //% number.defl=0
+    //% value.defl=0
+    //% channel.defl=1
+    export function controlChange(number: number, value: number, channel: number = null): string {
+        return "[" + (MIDIMessageType.ControlChange + validateChannel(channel) - 1) + "," + validateControlNumber(number) + "," + validateControlValue(value) + "]";
+    }
+
+    //% block="program change $number||channel $channel"
+    //% group="Commands"
+    //% weight=30
+    //% expandableArgumentMode="enabled"
+    //% number.defl=0
+    //% channel.defl=1
+    export function programChange(number: number, channel: number = null): string {
+        return "[" + (MIDIMessageType.ProgramChange + validateChannel(channel) - 1) + "," + validateProgramNumber(number) + "]";
+    }
+
+    //% block="channel pressure $pressure||channel $channel"
+    //% group="Commands"
+    //% weight=20
+    //% expandableArgumentMode="enabled"
+    //% pressure.defl=63
+    //% channel.defl=1
+    export function channelPressure(pressure: number, channel: number = null): string {
+        return "[" + (MIDIMessageType.ChannelPresure + validateChannel(channel) - 1) + "," + validatePressure(pressure) + "]";
+    }
+
+    //% block="pitch bend $pitch||channel $channel"
+    //% group="Commands"
+    //% weight=10
+    //% expandableArgumentMode="enabled"
+    //% pitch.defl=2000
+    //% channel.defl=1
+    export function pitchBend(pitch: number, channel: number = null): string {
+        let validatedPitch = validatePitch(pitch);
+
+        let lestSignificantPitch = validatedPitch & 127;
+        let mostSignificantPitch = validatedPitch >> 7;
+
+        return "[" + (MIDIMessageType.PitchBend + validateChannel(channel) - 1) + "," + lestSignificantPitch + "," + mostSignificantPitch + "]";
+    }
+
+    //% block="set global channel $channel"
+    //% group="Global Channel"
+    //% weight=20
+    //% channel.defl=1
+    export function setGlobalChannel(channel: number): void {
+        globalChannel = channel;
+    }
+
+    //% block="get global channel"
+    //% group="Global Channel"
+    //% weight=10
+    export function getGlobalChannel(): number {
+        return globalChannel;
+    }
+}
---
+++ MakeCode-Extension/README.md
@@ -0,0 +1,103 @@
+# Sonic Pi OSC Extension for the micro:bit
+
+This extension has been developed to allow a micro:bit to send OpenSound Control (OSC) messages directly to an instance of Sonic Pi.
+
+The extension is built around the [ESP8266](https://en.wikipedia.org/wiki/ESP8266) low cost serial Wi-Fi microchip built into [Elecfreaks](https://www.elecfreaks.com/store/) iot:bit module, although any ESP8266 micro:bit module should work but may require different pins to be selected in the initialise block. The module was purchased from [Cool Components](https://coolcomponents.co.uk/products/iot-bit-for-bbc-micro-bit) in the UK.
+
+![iot:bit module for the micro:bit by Elecfreaks](https://github.com/RBilsland/pxt-sonicpiosc/blob/master/images/iotbit.png)
+
+
+## Why?
+
+Why build this extension? Well Sonic Pi is a fantastic musical tool and micro:bits allow so many ways of interacting with them so getting them to talk to each other opens up a whole new world of possibilities. Music is something that should be able to be physically interacted with and the micro:bits provide so many ways of interacting with them either by themselves or through things being plugged into them. It's all about encouraging creativity and collaboration between people with different ideas and skill sets.
+
+
+## Demos
+
+I've written a couple of demos (including videos) using this extension, a Theremin demo where the pitch and roll of a micro:bit control the note and volumed played and a virtual drum kit where a micro:bit on each hand and one on a foot control the playing of a snare drum, kick drum and cymbal.
+
+* Theremin Demo - https://github.com/RBilsland/pxt-sonicpiosc/blob/master/ThereminDemo.md
+* Virtual Drum Kit Demo - https://github.com/RBilsland/pxt-sonicpiosc/blob/master/VirtualDrumKitDemo.md
+
+
+## What Next?
+
+I'm not sure, I have a few crazy ideas but nothing concrete yet, time to connect more things to a micro:bit I think. I'd love to see what you create too.
+
+
+## Using the Extension
+
+For this I will use the micro:bits Microsoft MakeCode block editor.
+
+1. Open the Microsoft MakeCode editor and create a new empty project.
+2. As this extension isn't included by default it will need to be added to the project. From the bottom of the palette of block categories click the Advanced section to expand it and then from the bottom of the Advanced section click Extensions.
+3. The extension isn't currently included in the extensions library so enter https://github.com/rBilsland/pxt-sonicpiosc in the search box at the top of the page and press return.
+4. Click the extension presented to add it to the project.
+
+
+## Connecting to Sonic Pi
+
+Before any messages can be sent to Sonic Pi a connection must first be established. The process involves 3 steps;
+
+### Initialising the connection to the ESP8266 microchip
+
+If using the iot:bit module then the default parameters will suffice. Once complete you can check if it was successful or not.
+
+![initialise block commands](https://github.com/RBilsland/pxt-sonicpiosc/blob/master/images/initialise.png)
+
+### Connecting to WiFi
+
+You will need to provide the details of the WiFi access point you wish to connect to. Make sure you enter upper / lower case characters correctly as this will make a difference. You cannot attempt a WiFi connection unless you have successfully initialised a connection first. Once complete you can check if it was successful or not.
+
+![wifi block commands](https://github.com/RBilsland/pxt-sonicpiosc/blob/master/images/connectWiFi.png)
+
+### Connecting to Sonic Pi
+
+You will need to provide the details of the IP Address of the machine where Sonic Pi is running and the port the service is running on. Both of these pieces of information can be found in the IO preferences of Sonic Pi. Also make sure both the Enable OSC server and Send/Receive remote OSC checkboxes are ticked. You cannot attempt a Sonic Pi connection unless you have successfully connected WiFi first. Once complete you can check if it was successful or not.
+
+![sonic pi block commands](https://github.com/RBilsland/pxt-sonicpiosc/blob/master/images/connectSonicPi.png)
+
+### Example of use
+
+This establishes a connection to Sonic Pi as the micro:bit starts. The additional 500ms delay is to allow the ESP8266 microchip to start. After each step status is checked before proceeding.
+
+![initialise example block commands](https://github.com/RBilsland/pxt-sonicpiosc/blob/master/images/initialiseExample.png)
+
+
+## Sending OpenSound Control (OSC) Messages
+
+Once a connection to Sonic Pi has been created messages can be sent.
+
+### Building a Message
+
+This process is broken down into 3 steps, starting a command with its associated address, adding parameters (if any are required) and finally sending the message. The address is what in Sonic Pi can be synced to and strings, integers and float can be passed as parameters. Once the message has been sent you can check if it was successful or not.
+
+![message block commands](https://github.com/RBilsland/pxt-sonicpiosc/blob/master/images/message.png)
+
+### Example of use
+
+When the A button is pressed a message will be built and sent with the address of "microbit" and will include 3 parameters, the first a "hello" string, next an integer of 123 and finally a float of 456.789.
+
+![message example block commands](https://github.com/RBilsland/pxt-sonicpiosc/blob/master/images/messageExample.png)
+
+
+## Additional Blocks
+
+There are a couple of additional behind the scenes parameters that can be adjusted. They are to do with the number of retries that are automatically attempted and the timeout duration (in milliseconds) when communicating with the ESP8266. By default the number of retries is set to 1 (so no retries, it just tries once) and timeout duration is set to 10000ms (or 10 seconds).
+
+![additional block commands](https://github.com/RBilsland/pxt-sonicpiosc/blob/master/images/additional.png)
+
+
+## License
+
+MIT
+
+
+## Supported targets
+
+* for PXT/microbit
+(The below metadata is required for package searching.)
+
+```package
+sonicpiosc=github:RBilsland/Microbit-Wireless-MIDI
+```
